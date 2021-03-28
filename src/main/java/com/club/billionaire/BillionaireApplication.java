de45ad2ad76f5fa5e309317167936c2b90002540
paliwalarpan <arpan.paliwal12@gmail.com>
2021-03-28 14:23:55 +0530

    added swagger 2

--- src/main/java/com/club/billionaire/BillionaireApplication.java
+++ src/main/java/com/club/billionaire/BillionaireApplication.java
@@ -2,8 +2,10 @@ package com.club.billionaire;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class BillionaireApplication {
 
     public static void main(String[] args) {
